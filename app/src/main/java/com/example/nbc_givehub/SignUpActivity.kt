b76c162557e5a329f759a3dc2834283bcabf7aab
JangGyuSik <jgs288@gmail.com>
2024-04-01 17:36:45 +0900

    회원가입 UI완성

---
+++ app/src/main/java/com/example/nbc_givehub/SignUpActivity.kt
@@ -0,0 +1,15 @@
+package com.example.nbc_givehub
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+
+
+
+    }
+}
